c81112b0cfc4e32d935fa0965310477cb1a502ab
oommk-zhao <lastsky_ok@163.com>
2020-11-01 22:20:44 +0800

    Add files via upload

--- EthernetSimulator/hpnetworksocket.h
+++ EthernetSimulator/hpnetworksocket.h
@@ -2,12 +2,29 @@
 #define HPNETWORKSOCKET_H
 
 #include <QObject>
+#include <QString>
 
 class HPNetworkSocket : public QObject
 {
     Q_OBJECT
 public:
+
+    enum HP_SOCKET_TYPE{
+        HP_DEFAULT_SOCKET = 1,
+        HP_UPD_SOCKET = 1,
+        HP_TCP_SERVER = 1 << 1,
+        HP_TCP_SOCKET = 1 << 2
+    } ;
+
     explicit HPNetworkSocket(QObject *parent = nullptr);
+    virtual ~HPNetworkSocket();
+
+    void initialized(void);
+
+    void createConnection(const QString& address, const QString& port, HPNetworkSocket::HP_SOCKET_TYPE socketType);
+
+
+
 
 signals:
 
